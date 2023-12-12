da09839a1c84ee6279063d82b64423bde493dcb9
anika-gup <143746022+anika-gup@users.noreply.github.com>
2023-12-12 11:35:31 -0800

    Add files via upload

---
+++ node.cpp
@@ -0,0 +1,27 @@
+#include "node.h"
+#include <iostream>
+#include <cstring>
+using namespace std;
+Node::Node(Student* s) : student(s), next(0) {}
+  //creating node, takes in a student pointer
+Node::~Node() {
+    // Assuming proper memory management for Student objects is handled elsewhere
+}
+
+void Node::setStudent(Student* s) {
+    //not needed
+    student = s;
+}
+
+void Node::setNext(Node* n) {
+    next = n;
+}
+
+Student* Node::getStudent() {
+    return student;
+}
+
+Node* Node::getNext() {
+    return next;
+}
+
